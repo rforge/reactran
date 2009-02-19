b695f564c87ece970ef424927f02b7149fd73606
filipm <filipm@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-02-19 19:03:47 +0000

    Error corrected in setup.2D.grid Adaptation of Rd file tran.2D
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/reactran@18 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ReacTran/R/transport.r
+++ pkg/ReacTran/R/transport.r
@@ -265,28 +265,29 @@ setup.prop.2D <- function(func = NULL,     # function
 {
 # check input
 gn <- names(grid)
-if (! "x.mid"  %in% gn) stop("error in setup.2Dprop: grid should be a list that contains x.mid")
-if (! "x.int"  %in% gn) stop("error in setup.2Dprop: grid should be a list that contains x.int")
-if (! "y.mid"  %in% gn) stop("error in setup.2Dprop: grid should be a list that contains x.mid")
-if (! "y.int"  %in% gn) stop("error in setup.2Dprop: grid should be a list that contains x.int")
+if (! "x.mid"  %in% gn) stop("error in setup.2D.prop: grid should be a list that contains x.mid")
+if (! "x.int"  %in% gn) stop("error in setup.2D.prop: grid should be a list that contains x.int")
+if (! "y.mid"  %in% gn) stop("error in setup.2D.prop: grid should be a list that contains x.mid")
+if (! "y.int"  %in% gn) stop("error in setup.2D.prop: grid should be a list that contains x.int")
 
 if (is.null(func) && is.null(value)) stop("error in setup.prop: function and value should not be both NULL")
-
-x.int <- matrix(nrow=length(grid$x.int),ncol=length(grid$y.mid))
-y.int <- matrix(nrow=length(grid$x.mid),ncol=length(grid$y.int))
-mid <- matrix(nrow=length(grid$x.mid),ncol=length(grid$y.mid))
 
 if (!is.null(value))
 # profile specification via value
 {
-x.int <- value
-y.int <- value 
-mid <- value
+x.int <- matrix(nrow=length(grid$x.int),ncol=length(grid$y.mid),data=value)
+y.int <- matrix(nrow=length(grid$x.mid),ncol=length(grid$y.int),data=value) 
+mid <- matrix(nrow=length(grid$x.mid),ncol=length(grid$y.mid),data=value)
 }
 
 if (!is.null(func))
 # profile specification via function 
 {
+
+x.int <- matrix(nrow=length(grid$x.int),ncol=length(grid$y.mid))
+y.int <- matrix(nrow=length(grid$x.mid),ncol=length(grid$y.int))
+mid <- matrix(nrow=length(grid$x.mid),ncol=length(grid$y.mid))
+
 for (i in 1:length(grid$x.int)) x.int[i,] <- func(grid$x.int[i],grid$y.mid,...)
 for (i in 1:length(grid$x.mid)) y.int[i,] <- func(grid$x.mid[i],grid$y.int,...)
 for (i in 1:length(grid$x.mid)) mid[i,] <- func(grid$x.mid[i],grid$y.mid,...)
