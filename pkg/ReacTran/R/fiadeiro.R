e7e57d0150451841b04c133c0b027260a7f5bb2e
filipm <filipm@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-03-26 20:40:11 +0000

    dx.aux = NULL in fiadeiro.R + changes in Rd files
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/reactran@28 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ReacTran/R/fiadeiro.R
+++ pkg/ReacTran/R/fiadeiro.R
@@ -1,9 +1,9 @@
 
 ##==============================================================================
-## The fiadeiro and veronis scheme: upstream weighing cofficients
+## The Fiadeiro and Veronis scheme: advective finite difference weights (AFDW)
 ##==============================================================================
 
-fiadeiro <- function(v, D, dx.aux, grid=list(dx.aux=dx.aux)) {
+fiadeiro <- function(v, D, dx.aux=NULL, grid=list(dx.aux=dx.aux)) {
 
   Pe    <- abs(v)*grid$dx.aux/D
   sigma <- (1+(1/tanh(Pe)-1/Pe))/2
